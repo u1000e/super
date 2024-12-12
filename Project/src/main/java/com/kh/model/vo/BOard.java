e06cba1b1075afb0399b28653d843e42d5d27a0e
BlueTree-Na <ncs990808@gmail.com>
2024-12-12 10:22:09 +0900

    보드만듬 - 승철

---
+++ Project/src/main/java/com/kh/model/vo/BOard.java
@@ -0,0 +1,22 @@
+package com.kh.model.vo;
+
+public class BOard {
+
+	
+	private int age;
+
+	public BOard() {
+		super();
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
+}
