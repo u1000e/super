9a4391837d3ec60fddcfaecd42db576be0796c18
u1000e <73210038+u1000e@users.noreply.github.com>
2024-12-12 10:11:16 +0900

    멤버 생성자랑 게터세터 만듬 -이승철-

--- Project/src/main/java/com/kh/model/vo/Member.java
+++ Project/src/main/java/com/kh/model/vo/Member.java
@@ -4,5 +4,32 @@ public class Member {
 	
 	private String member;
 	private String memberPwd;
+	
+	public Member() {
+		super();
+	}
+	public Member(String member, String memberPwd) {
+		super();
+		this.member = member;
+		this.memberPwd = memberPwd;
+	}
+	public String getMember() {
+		return member;
+	}
+	public void setMember(String member) {
+		this.member = member;
+	}
+	public String getMemberPwd() {
+		return memberPwd;
+	}
+	public void setMemberPwd(String memberPwd) {
+		this.memberPwd = memberPwd;
+	}
+	@Override
+	public String toString() {
+		return "Member [member=" + member + ", memberPwd=" + memberPwd + "]";
+	}
+	
+	
 
 }
