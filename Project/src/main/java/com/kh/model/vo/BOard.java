05257af5c52f498719cbebf4b957cc2f1b89357c
u1000e <73210038+u1000e@users.noreply.github.com>
2024-12-12 10:26:01 +0900

    Revert "Revert "어쩌고저쩌고~~~""
    This reverts commit 5089cd435fd16e0c2a48e08c106a3656fa27cdef.

--- Project/src/main/java/com/kh/model/vo/BOard.java
+++ Project/src/main/java/com/kh/model/vo/BOard.java
@@ -2,8 +2,17 @@ package com.kh.model.vo;
 
 public class BOard {
 
-	
 	private int age;
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 	public BOard() {
 		super();
